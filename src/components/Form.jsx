f7700fd0c78c888f704f9585626eb30c526b931f
omorsultan <omorsultansust@gmail.com>
2026-08-29 11:43:10 +0600

    now show in console but not window

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -5,14 +5,21 @@ export default class Form extends React.Component {
   state = {
     title: 'javaScript'
   }
+
+  handleChange = (e) =>{
+    console.log(e.target.value);
+    
+  }
   render(){
     const {title} = this.state;
     return(
       <div>
         <form>
-          <input type = "text"  placeholder='Enter Title'
-          value={title}
-          className='border-2 border-gray-300 rounded-sm  focus:border-blue-500 mt-6'/>
+          <input type = "text" 
+           placeholder='Enter Title'
+           value={title}
+           onChange={this.handleChange}
+           className='border-2 border-gray-300 rounded-sm  focus:border-blue-500 mt-6'/>
         </form>
       </div>
     )
