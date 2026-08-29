5eeee7e45faee0ba517af6874787ee6c396f44ba
omorsultan <omorsultansust@gmail.com>
2026-08-29 11:39:07 +0600

    But cant change now

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 
 export default class Form extends React.Component {
+
+  state = {
+    title: 'javaScript'
+  }
   render(){
+    const {title} = this.state;
     return(
       <div>
         <form>
           <input type = "text"  placeholder='Enter Title'
+          value={title}
           className='border-2 border-gray-300 rounded-sm  focus:border-blue-500 mt-6'/>
         </form>
       </div>
