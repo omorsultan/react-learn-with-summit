bd60c808bf4849b2b844fff0377c22c1fd869fa8
omorsultan <omorsultansust@gmail.com>
2026-08-29 07:36:09 +0600

    this is with comment best . previous no need

--- src/components/Clock.jsx
+++ src/components/Clock.jsx
@@ -1,42 +1,44 @@
 import React from 'react'
 
-class Clock extends  React.Component{
-  state ={
+class Clock extends React.Component {
+
+  state = {
     date: new Date(),
     locale: 'bn-BD'
-  }
+  } // default value of state
 
-  componentDidMount(){
-    this.clockTimer = setInterval(()=>{
+  componentDidMount() { // runs after the component is mounted/rendered
+    this.clockTimer = setInterval(() => {
       this.tick();
-    },1000);
+    }, 1000) // calls tick() every 1 second
   }
 
-  componentWillUnmount(){
-    clearInterval(this.clockTimer);
+  componentWillUnmount() { // runs before the component is removed
+    clearInterval(this.clockTimer); // stops the timer
   }
 
-  tick = ()=>{
-    this.setState({
-      date: new Date(),
-    })
+  handleClick = () => { // arrow function keeps the correct 'this' context
+    this.setState({ // proper way to update state
+      locale: 'en-US',
+    });
   }
 
-  handleClick = () =>{
-    this.setState({
-      locale: 'en-US'
-    })
+  tick = () => {
+    this.setState({ date: new Date() }); // updates the current time
   }
 
-  render(){
-    const {date, locale} = this.state;
+  render() {
+    const { date, locale } = this.state;
 
-    return <>
-    {date.toLocaleTimeString(locale)};
-    <button type="button" onClick={this.handleClick}>
-      click to change Language
-    </button>
-    </>
+    return (
+      <>
+        {date.toLocaleTimeString(locale)}
+
+        <button type="button" onClick={this.handleClick}>
+          Change the language
+        </button>
+      </>
+    )
   }
 }
 
