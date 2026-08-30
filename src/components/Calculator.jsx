3e7d67e46116d7b715298eb6f41eb18b44435422
omorsultan <omorsultansust@gmail.com>
2026-08-30 19:36:18 +0600

    working but take data from different source

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -1,35 +1,17 @@
 import React from 'react'
 import BoilingVerdict from './BoilingVerdict'
+import InputTemperature from './InputTemperature'
 
 
 class Calculator  extends React.Component{
   
-  state = {
-    temperature: '',
-  }
-
-  onTemperatureChange = (e) =>{
-    this.setState({
-      temperature: e.target.value,
-    })
-  }
-
   render(){
-    const {temperature } = this.state;
 
     return(
       <>
-      <div className='bg-amber-300 mt-10 ml-20 mr-20'>
-         <fieldset >
-       <legend>Enter temperature in Celsius</legend>
-       <input type='text' value={temperature} 
-       onChange={this.onTemperatureChange}
-       className='bg-green-400 border-blue-700 border-2' />
-      </fieldset>
-      <BoilingVerdict celsius={parseFloat(temperature)}/>
+      <InputTemperature scale="c" />
+      <InputTemperature scale="f" />
 
-      </div>
-     
       </>
     )
     
